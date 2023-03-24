962783f06f3d8b20eac42b5499b6be0626773b39
Bobo_alcazar <Bobo_alcazar@outlook.com>
2023-03-25 02:58:04 +0800

    complete basic test

--- classifier.py
+++ classifier.py
@@ -7,6 +7,7 @@ with open ('corpus/segmented.json', 'r', encoding='utf-8') as f :
 with open ('stopWords.json', 'r', encoding='utf-8') as f :
     stopWords:list[str] = json.load(f)
 
+
 def stop (raw:dict) -> dict :
     #輸入爲分詞文檔，應輸出相同的東西，但去除停用詞。
     newDict:dict = {}
@@ -15,7 +16,14 @@ def stop (raw:dict) -> dict :
         newDict[key] = [value[0], value[1], cooked]
     return newDict
 
-doc_train, doc_test, sect_train, sect_test, author_train, author_test = None
+def list2str (raw:dict) -> dict :
+    ripe:dict = {}
+    for key, value in raw.items() :
+        ripe[key] = [value[0], value[1], ' '.join(value[2])]
+    return ripe
+
+
+doc_train = doc_test = sect_train = sect_test = author_train = author_test = None
 def seperate (raw:dict) :
     '''
     從文檔列表劃分訓練集和測試集，返回派別、作者、詞袋的訓練集與測試集。
@@ -30,6 +38,7 @@ def seperate (raw:dict) :
     from sklearn.model_selection import train_test_split
     global doc_train, doc_test, sect_train, sect_test, author_train, author_test
     doc_train, doc_test, sect_train, sect_test, author_train, author_test = train_test_split(doc, sect, author, test_size=0.3, random_state=10023, shuffle=True)
+    #doc_train, doc_test, sect_train, sect_test = train_test_split(doc, sect, test_size=0.3, random_state=10023, shuffle=True)
 
 def makeVector (fang:str) :
     global doc_train, doc_test
@@ -98,16 +107,17 @@ def report (x_train, y_train, x_test, y_test, y_classified) :
     print(roc_auc_score(y_test, clf.predict(x_test)))
 
 
-def main (ifStop:bool, repre:str, ifSift=False, algo) :
-    global doc_train, doc_test, sect_train, sect_test, author_train, author_test
+def main (ifStop:bool, repre:str, ifSift=False, algo=None) :
+    global doc_train, doc_test, sect_train, sect_test, author_train, author_test, corpus
     if ifStop == True :
         corpus = stop(corpus)
+    corpus = list2str(corpus)
     seperate(corpus)
     doc_train, doc_test = makeVector (repre)
     if ifSift != False :
         doc_train, doc_test = sift (ifSift, doc_train, doc_test, sect_train)
     print('完成降維')
-    sect_classified (doc_train, sect_train, doc_test, sect_test, algo)
+    sect_classified = classify(doc_train, sect_train, doc_test, sect_test, algo)
     report (doc_train, sect_train, doc_test, sect_test, sect_classified)
     
 
